93c8e67aeeb0aee54e406ed85619ef4ac20db43e
Iker-Romero <ikersoftdev@gmail.com>
2023-08-29 17:00:30 +0200

    Hero gaps adjusted

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -6,19 +6,19 @@ import s from './Hero.module.scss'
 const Hero = () => {
   return (
     <section className={s['hero']}>
-      <div className={s['text-container']}>
-        <h1>Iker Romero</h1>
-        <div className={s.content}>
+      <div className={s.content}>
+        <div className={s['text-container']}>
+          <h1>Iker Romero</h1>
           <p>
             Transforming Ideas into Digital Realities. Whether you need a
             stunning landing page or a complex web application, I create
             solutions that fit your vision. Let&apos;s build something
             extraordinary together.
           </p>
-          <Link href="/contact" variant="callToAction">
-            Let&apos;s Talk
-          </Link>
         </div>
+        <Link href="/contact" variant="callToAction">
+          Let&apos;s Talk
+        </Link>
       </div>
       <div className={s['image-wrapper']}>
         <Image
