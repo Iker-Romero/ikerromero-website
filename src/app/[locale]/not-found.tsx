c31303e231e4e358b4f100a3aac681e0b97f3844
Iker-Romero <ikersoftdev@gmail.com>
2024-01-28 10:05:58 +0100

    404 page localization

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,19 +1,19 @@
 import H1 from '@/components/H1/H1'
 import Link from '@/components/Link/Link'
+import { useTranslations } from 'next-intl'
 
 const NotFound404 = () => {
+  const t = useTranslations('404NotFound')
+
   return (
     <div>
-      <H1>404</H1>
-      <p>
-        The page you are looking for might have been removed, had its name
-        changed, or is temporarily unavailable.
-      </p>
+      <H1>{t('title')}</H1>
+      <p>{t('description')}</p>
       <br />
       <p>
-        Go back to{' '}
+        {t('redirect.indication')}
         <Link href="/#" variant="underlined">
-          homepage
+          {t('redirect.destination')}
         </Link>
         .
       </p>
