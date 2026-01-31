98ee77eacb96131e8eec1106f31bfd6816739aa8
Iker Romero <ikersoftdev@gmail.com>
2026-01-31 16:04:52 +0100

    Fix PostHog init: move to useEffect for Next.js SSR

--- src/utils/PostHogProvider.tsx
+++ src/utils/PostHogProvider.tsx
@@ -2,14 +2,21 @@
 
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
-import { ReactNode } from 'react'
-
-if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com'
-  })
-}
+import { ReactNode, useEffect } from 'react'
 
 export function PHProvider({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host:
+          process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+        person_profiles: 'identified_only',
+        loaded: posthog => {
+          if (process.env.NODE_ENV === 'development') posthog.debug()
+        }
+      })
+    }
+  }, [])
+
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
