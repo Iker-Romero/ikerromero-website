265118400732d96f84ca25be611808d1732ed4ad
Iker Romero <ikersoftdev@gmail.com>
2026-01-31 17:26:30 +0100

    Add NEXT_PUBLIC_ENABLE_POSTHOG toggle to disable analytics locally

--- src/utils/PostHogProvider.tsx
+++ src/utils/PostHogProvider.tsx
@@ -4,9 +4,9 @@ import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 import { ReactNode } from 'react'
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isEnabled = process.env.NEXT_PUBLIC_ENABLE_POSTHOG === 'true'
 
-if (isProduction && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+if (isEnabled && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     ui_host: 'https://eu.posthog.com',
@@ -15,7 +15,7 @@ if (isProduction && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
 }
 
 export function PHProvider({ children }: { children: ReactNode }) {
-  if (!isProduction) {
+  if (!isEnabled) {
     return children
   }
 
